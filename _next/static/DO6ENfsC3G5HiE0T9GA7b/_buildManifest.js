c2c90a873380f7db94bd671556f4b8e1f166cf31
alongor666 <alongor666@users.noreply.github.com>
2025-12-08 00:55:51 +0000

    deploy: 6da86d64bb88ec7f011b7d6a2f0dbf60cc819302

---
+++ _next/static/DO6ENfsC3G5HiE0T9GA7b/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-f8b39a9fd947f115.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
